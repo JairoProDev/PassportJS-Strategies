1c129283cd8efb85a707f2adbc2b2bcd618dc405
JairoProDev <JairoProDev@gmail.com>
2023-04-29 23:00:24 -0500

    registrarse-failed

--- username-password-tutorial/routes/auth.js
+++ username-password-tutorial/routes/auth.js
@@ -49,3 +49,45 @@ router.post('/logout', function(req, res, next) {
     });
 });
 // Path: routes\index.js
+// router.get('/signup', function(req, res, next) {
+//     res.render('signup');
+// });
+
+// router.post('/signup', function(req, res, next) {
+//     crypto.randomBytes(32, function(err, salt) {
+//       if (err) { return next(err); }
+  
+//       crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+//         if (err) { return next(err); }
+  
+//         db.run('INSERT INTO users (username, salt, hashed_password) VALUES (?, ?, ?)', [ req.body.username, salt, hashedPassword ], function(err) {
+//           if (err) { return next(err); }
+  
+//           res.redirect('/login');
+//         });
+//       });
+//     });
+//   }
+// }
+
+router.post('/signup', function(req, res, next) {
+  var salt = crypto.randomBytes(16);
+  crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+    if (err) { return next(err); }
+    db.run('INSERT INTO users (username, hashed_password, salt) VALUES (?, ?, ?)', [
+      req.body.username,
+      hashedPassword,
+      salt
+    ], function(err) {
+      if (err) { return next(err); }
+      var user = {
+        id: this.lastID,
+        username: req.body.username
+      };
+      req.login(user, function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+    });
+  });
+});
